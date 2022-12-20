c237c4bdbe58b31652bc51d2551303e231c7ba72
mallumoviesearch <103921432+mallumoviesearch@users.noreply.github.com>
2022-12-20 13:09:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ if os.path.exists(".env"):
     load_dotenv(".env")
     
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-SESSION = os.getenv("SESSION")
+API_ID = int(os.getenv("API_ID", "15352099"))
+API_HASH = os.getenv("API_HASH", "a5fddd08a3d1375fef2eef6d3e8a8186")
+SESSION = os.getenv("SESSION"‚ "AQCufBi6WZQVhznPt4k60AwqIgPoWAOQpxD_XOwrnHLE_GCfFyItAnpFs2XuxRY4YCwgmHlBv_NhXQvf0mYv4iKs9rXedFYjsv2Uz0SVk4OMHzFNumIUU5IDbNjsZ1jW3Fi4ZM40wq06AZJC7zqGA2s3zZycHtFJ5Ywn-3w0WJi_xzWgOCx8XQRcXn953u-pGeNdztCiSmM5kEzNpl5Cxp14nO9iFBBP9fiptTLyTv3QxWzsST9g4DNJ5oQ8xYAyMiUNyoKoVDSjcyGoF7dgjoFq-QBrvWIlGOymd9IPds9t1H309_zWtO4swfl3AT-NEiizBXz4NMI9LP_ofB0eZbr-AAAAATZe0mAA")
 HNDLR = os.getenv("HNDLR", "!")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
